141d363a49ee455f86ace35597fd79f7b5f3d418
こた <tokyosato1@gmail.com>
2023-07-01 16:16:59 +0900

    boxshadow

--- src/components/tab/Support.tsx
+++ src/components/tab/Support.tsx
@@ -1,60 +1,45 @@
 import { ExLink, TabContainer } from '@/components/ChakraExtension';
 import { socialLink } from '@/lib/consts';
 import design from '@/lib/design';
-import { Box, HStack, Image, Text, useMediaQuery } from '@chakra-ui/react';
+import { HStack, Image, Text, useMediaQuery } from '@chakra-ui/react';
 
 const Support = () => {
     const [isLargerThan480] = useMediaQuery('(min-width: 480px)');
 
     return (
         <TabContainer>
-            <Box p={2}>
-                <Box mb={4}>
-                    <Text
-                        fontSize='3xl'
-                        fontWeight='bold'
-                        bgGradient={design.graduation.blue2}
-                        bgClip='text'
-                    >
-                        Thank you for seeing this tab!
-                    </Text>
-                    <Text
-                        fontSize='xl'
-                        fontWeight='bold'
-                        bgGradient={design.graduation.gb}
-                        bgClip='text'
-                    >
-                        You can help me effortlessly
-                    </Text>
-                </Box>
-                <TabContainer mb={4}>
-                    <HStack>
-                        <ExLink href={socialLink.buyMeACoffee}>
-                            <Image
-                                src='https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png'
-                                alt='Buy Me A Coffee'
-                                h='82px'
-                                w='300px'
-                            />
-                        </ExLink>
-                        {isLargerThan480 && (
-                            <Box ml={10}>
-                                <Image
-                                    src='/images/buymeacoffeeqr.png'
-                                    alt='Buy Me A Coffee QR Code'
-                                    h={100}
-                                    w={100}
-                                />
-                            </Box>
-                        )}
-                    </HStack>
-                </TabContainer>
-                <TabContainer>
-                    <ExLink href={socialLink.paypal}>
-                        <Image src='/images/paypal.png' alt='Paypal' h='120px' w='240px' />
-                    </ExLink>
-                </TabContainer>
-            </Box>
+            <Text
+                fontSize='3xl'
+                fontWeight='bold'
+                bgGradient={design.graduation.blue2}
+                bgClip='text'
+            >
+                Thank you for seeing this tab!
+            </Text>
+            <Text fontSize='xl' fontWeight='bold' bgGradient={design.graduation.gb} bgClip='text'>
+                You can help me effortlessly
+            </Text>
+            <HStack>
+                <ExLink href={socialLink.buyMeACoffee}>
+                    <Image
+                        src='https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png'
+                        alt='Buy Me A Coffee'
+                        h='82px'
+                        w='300px'
+                    />
+                </ExLink>
+                {isLargerThan480 && (
+                    <Image
+                        src='/images/buymeacoffeeqr.png'
+                        alt='Buy Me A Coffee QR Code'
+                        h={100}
+                        w={100}
+                    />
+                )}
+            </HStack>
+            <ExLink href={socialLink.paypal}>
+                <Image src='/images/paypal.png' alt='Paypal' h='100px' w='200px' />
+            </ExLink>
         </TabContainer>
     );
 };
