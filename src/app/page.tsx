cc9c27913f97af3192a57be271e9bf6df5a6bf5b
こた <tokyosato1@gmail.com>
2024-03-02 20:15:31 +0900

    Refactor Flex component to use HStack and Stack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,24 @@
 import ProfileCard from '@/components/ProfileCard';
 import TabCard from '@/components/TabCard';
 import design from '@/lib/design';
-import { Flex } from '@chakra-ui/react';
+import { HStack, Stack } from '@chakra-ui/react';
 
 const Home = () => {
     return (
-        <Flex bgGradient={design.graduation.gb} justify='center' minH='96vh' py='2vh'>
-            <Flex
-                direction={['column', 'column', 'row']}
+        <HStack bgGradient={design.graduation.gb} justify='center' minH='96vh' py='2vh'>
+            <Stack
+                direction={{ base: 'column', md: 'row' }}
+                p={{ base: 1, md: 4 }}
                 w='96%'
                 minH='96vh'
                 bg='white'
                 borderRadius='lg'
                 shadow='lg'
-                p={[1, 1, 4]}
             >
                 <ProfileCard />
                 <TabCard />
-            </Flex>
-        </Flex>
+            </Stack>
+        </HStack>
     );
 };
 
